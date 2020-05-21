7c77ada6788a7e82b15e4e20c4535520bcc70ce7
lin <1831518139@qq.com>
2020-05-21 16:45:31 +0800

    新增计算两个数值功能

--- demo01.py
+++ demo01.py
@@ -1,2 +1,12 @@
 print("这是develop中的demo01")
 
+
+def avg(num1, num2):
+    """计算两个数字的平均数"""
+    avg_num = (num1+num2)/2
+    print(f"{num1}和{num2}的平均数是{avg_num}")
+
+
+new_num1 = float(input("请输入第一个数字："))
+new_num2 = float(input("请输入第二个数字："))
+avg(new_num1, new_num2)
